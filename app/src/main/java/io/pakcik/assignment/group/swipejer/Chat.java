d1959ab855f61cb265cd38560147555b78a91d8e
alfdnl <42483827+alfdnl@users.noreply.github.com>
2021-01-22 00:33:38 +0800

    Innitial Commit

---
+++ app/src/main/java/io/pakcik/assignment/group/swipejer/Chat.java
@@ -0,0 +1,15 @@
+package io.pakcik.assignment.group.swipejer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat);
+    }
+}
---
+++ app/src/main/java/io/pakcik/assignment/group/swipejer/arrayAdapter.java
@@ -0,0 +1,46 @@
+package io.pakcik.assignment.group.swipejer;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+import java.util.List;
+
+
+public class arrayAdapter extends ArrayAdapter<cards>{
+
+    Context context;
+
+    public arrayAdapter(Context context, int resourceId, List<cards> items){
+        super(context, resourceId, items);
+    }
+    public View getView(int position, View convertView, ViewGroup parent){
+        cards card_item = getItem(position);
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+        }
+
+        TextView name = (TextView) convertView.findViewById(R.id.name);
+        ImageView image = (ImageView) convertView.findViewById(R.id.image);
+
+
+        name.setText(card_item.getName());
+        byte[] foodImage = card_item.getImage();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(foodImage, 0, foodImage.length);
+        image.setImageBitmap(bitmap);
+
+
+
+        return convertView;
+
+    }
+}
