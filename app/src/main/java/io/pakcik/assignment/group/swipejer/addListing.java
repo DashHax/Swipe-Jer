eeb0aa6c646069f38c2b858c418e34d3ce369671
O'Dell Obrien <dashhax@gmail.com>
2021-01-23 10:27:01 +0800

    Fixed SharedPreference error

--- app/src/main/java/io/pakcik/assignment/group/swipejer/addListing.java
+++ app/src/main/java/io/pakcik/assignment/group/swipejer/addListing.java
@@ -11,6 +11,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -66,7 +67,7 @@ public class addListing  extends AppCompatActivity implements AdapterView.OnItem
         });
 
         if (shp == null)
-            shp = getSharedPreferences("myPreferences", MODE_PRIVATE);
+            shp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         String user_id = shp.getString("id", "");
         final int userid = Integer.parseInt(user_id);
